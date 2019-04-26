86399c924e0d3ed828215252c43038ecbb6c79c1
mumo-dev <samumo.dev@gmail.com>
2019-04-26 12:42:51 +0300

    time formatting and some styling

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import moment from "moment";
 import "./newsItem.css";
 
 class NewsItem extends Component {
@@ -12,21 +13,24 @@ class NewsItem extends Component {
     // console.log(this.props);
     const story = this.props.story;
     return (
-      <div className="list-group-item rounded-0 border-right-0 border-left-0">
+      <div
+        className="list-group-item list-group-item-action rounded-0 border-right-0 border-left-0"
+        style={{ cursor: "pointer" }}
+      >
         <div className="d-flex">
-          <div className="news-item-title">{this.props.index}</div>
+          <div className="news-item-title">{this.props.index}.</div>
           <div className="ml-1">
             <section className="news-item-title ">{story.title}</section>
 
             <section className="news-item-url ">
-              <a href={story.url} className="">
+              <a href={story.url} target="_blank" rel="noopener noreferrer">
                 {story.url.slice(8)}
               </a>
             </section>
 
             <section className="news-item-footer">
-              {story.score} Points by {story.by} at 11.00 hrs{" "}
-              <span> &#8226;</span> {"  "}
+              {story.score} Points by {story.by} -{" "}
+              {moment(story.time * 1000).fromNow()} <span> &#8226;</span> {"  "}
               {story.kids.length} comments
             </section>
           </div>
